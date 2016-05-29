f0179b732742d29c6a57b4b49f5741a5af0e30fa
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-05-29 11:27:05 -0600

    updated workout list

--- client/components/workout_list.js
+++ client/components/workout_list.js
@@ -4,6 +4,9 @@ import {createContainer} from 'meteor/react-meteor-data';
 import {Workouts} from '../../imports/collections/workouts';
 import WorkoutDetail from './workout_detail';
 
+const PER_PAGE = 20;
+
+// TODO: dynamically update workout records on button click
 const WorkoutList = (props) => {
 	console.log(props.workouts);
 	return (
@@ -11,13 +14,17 @@ const WorkoutList = (props) => {
 			<div className="workout-list">
 				{props.workouts.map(workout => <WorkoutDetail key={workout._id} workout={workout}/>)}
 			</div>
+			<button onClick={() => Meteor.subscribe('workouts', 40)}
+							className="btn btn-primary">
+				Load more...
+			</button>
 		</div>
 	);
 };
 
 export default createContainer(() => {
 	// set up subscription
-	Meteor.subscribe('workouts');
+	Meteor.subscribe('workouts', PER_PAGE);
 
 	// return an object, sent to WorkoutList as props
 	return {workouts: Workouts.find({}).fetch()};
--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,6 @@ import { Meteor } from 'meteor/meteor';
 
 import {Workouts} from '../imports/collections/workouts';
 
-Meteor.publish('workouts', () => {
-	return Workouts.find({}, {limit: 20});
+Meteor.publish('workouts', (per_page) => {
+	return Workouts.find({}, {limit: per_page});
 });
